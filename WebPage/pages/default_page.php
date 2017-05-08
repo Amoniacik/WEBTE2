43f55f2cb26734d5bf130d0127aa3421ff6c7ea2
Tomas <rencestomas@gmail.com>
2017-05-08 16:12:10 +0200

    default page - zaklad pre podstranku

---
+++ WebPage/pages/default_page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+Template Name: Education Time
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>ÚAM FEI</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="../layout/styles/layout.css" type="text/css" />
+    <script type="text/javascript" src="../layout/scripts/jquery.min.js"></script>
+    <script type="text/javascript" src="../scripts/tables.js"></script>
+    <script type="text/javascript" src="../layout/scripts/slimbox2.js"></script>
+    <link rel="stylesheet" href="../layout/styles/slimbox2.css" type="text/css" media="screen" />
+    <!-- liteAccordion is Homepage Only -->
+    <link rel="stylesheet" href="../layout/scripts/liteaccordion-v2.2/css/liteaccordion.css" type="text/css" />
+</head>
+<body>
+<div id="top" onload="init()">
+    <div class="wrapper row4">
+        <!--TODO-->
+    </div>
+</div>
+</body>
+</html>
